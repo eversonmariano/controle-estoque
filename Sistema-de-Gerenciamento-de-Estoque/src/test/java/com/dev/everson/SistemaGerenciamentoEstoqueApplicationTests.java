7e5d0b69a73acbdee2fc5ea8edf83d267d746b40
eversonmariano <mariano.computacao@gmail.com>
2025-03-30 21:05:16 -0300

    ProjetoCriado+Dependencias+Bdconectado+estruturaPastas

---
+++ Sistema-de-Gerenciamento-de-Estoque/src/test/java/com/dev/everson/SistemaGerenciamentoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.everson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
